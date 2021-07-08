0613895c79ac43393a87fae048dac485257ee504
Leo Gagnon <leognon@gmail.com>
2021-07-07 22:14:43 -0400

    Refactor and added comments

--- vscExtension/bad-copilot/extension.js
+++ vscExtension/bad-copilot/extension.js
@@ -2,69 +2,81 @@ const vscode = require('vscode');
 const axios = require('axios');
 
 function activate(context) {
-	let selectedEditor;
-	let selectedRange;
+	let selectedEditor; //The editor to insert the completion into
+	let selectedRange; //The range to insert the completion into
 
-	context.subscriptions.push(vscode.commands.registerCommand('clone-pilot.insertCompletion', async () => {
-		// Get the active text editor
+	//A command to open the ClonePilot window
+	context.subscriptions.push(vscode.commands.registerCommand('clone-pilot.openClonePilot', async () => {
 		const editor = vscode.window.activeTextEditor;
-		if (editor) {
-			const document = editor.document;
-			let selection = editor.selection;
-
-			if (editor.selection.isEmpty) {
-				const cursorWordRange = editor.document.getWordRangeAtPosition(editor.selection.active);
-				if (!cursorWordRange) {
-					console.log('No cursor word range');
-					return;
-				}
-
-				selection = new vscode.Selection(cursorWordRange.start.line, cursorWordRange.start.character, cursorWordRange.end.line, cursorWordRange.end.character);
-			}
-
-			selectedEditor = editor;
-			selectedRange = selection;
-
-			// Get the word within the selection
-			const word = document.getText(selection);
-
-			try {
-				const response = await axios.get(`http://127.0.0.1:3000/getFunction/${word}`);
-				const fns = response.data.sort((a, b) => b.postScore - a.postScore);
-				await openVirtualDoc(fns);
-			} catch (err) {
-				console.log('Error sending request', err);
-			}
+		if (!editor) {
+			vscode.window.showInformationMessage('Please open an editor to use ClonePilot.');
+			return;
 		}
+
+		const document = editor.document;
+		let selection = editor.selection;
+
+		if (editor.selection.isEmpty) { //If nothing is highlited, get the word at the cursor
+			const cursorWordRange = editor.document.getWordRangeAtPosition(editor.selection.active);
+			if (!cursorWordRange) {
+				vscode.window.showInformationMessage('Please select or place your cursor on a word to use ClonePilot');
+				return; //Cursor not on a word
+			}
+
+			selection = new vscode.Selection(cursorWordRange.start.line, cursorWordRange.start.character, cursorWordRange.end.line, cursorWordRange.end.character);
+		}
+
+		selectedEditor = editor; //Save to be used when the completion is inserted
+		selectedRange = selection;
+
+		const word = document.getText(selection); //The word in the selection
+		await openClonePilot(word); //TODO Make a loading window pop up
+
 	}));
 
-
 	const myScheme = 'clonePilot';
-	const textDocumentProvider = new class {
-		provideTextDocumentContent(uri) {
-			return uri.path;
+	const textDocumentProvider = new class { //Provides a text document for the window
+		async provideTextDocumentContent(uri) {
+			const queryParams = uri.query.split('=');
+			if (queryParams.length < 2) return 'Something went wrong. Make sure a word is selected.';
+			const word = queryParams[1]; //Gets asdf from 'word=asdf'
+			try {
+				const response = await axios.get(`http://127.0.0.1:3000/getFunction/${word}`); //Get the functions for that word
+				const fns = response.data.sort((a, b) => b.postScore - a.postScore); //Show the highset score first
+				const content = getClonePilotText(fns, word);
+				return content;
+			} catch (err) {
+				console.log('Error sending request', err);
+				return 'There was an error sending the request\n' + err;
+			}
 		}
 	}();
 	context.subscriptions.push(vscode.workspace.registerTextDocumentContentProvider(myScheme, textDocumentProvider));
 
-	const openVirtualDoc = async (fns) => {
-		codelensProvider.clearPositions();
-		let content = `/* Clone Pilot found ${fns.length} functions */\n\n`;
-		for (let i = 0; i < fns.length; i++) {
-			const lineNum = content.split('\n').length;
-			const formattedFn = formatFunction(fns[i]);
-			codelensProvider.addPosition(lineNum, fns[i]);
-			content += formattedFn.postHeader + formattedFn.keywords + formattedFn.header + formattedFn.body;
-			if (i < fns.length - 1) content += '\n\n';
-		}
-		let uri = vscode.Uri.parse(myScheme + ':' + content); //TODO Don't pass the content through the URI. If there is a ? it will be in the query part
+	//Open the ClonePilot window to display the functions
+	const openClonePilot = async (word) => {
+		//A uri to send to the document
+		let uri = vscode.Uri.parse(`${myScheme}:Clone Pilot?word=${word}`, true); //TODO Don't pass the content through the URI. If there is a ? it will be in the query part
 		const doc = await vscode.workspace.openTextDocument(uri); // calls back into the provider
 		await vscode.window.showTextDocument(doc, {
 			viewColumn: vscode.ViewColumn.Beside,
-			preview: true,
+			preview: true, //Don't replace the current window
 			preserveFocus: true,
 		});
-		vscode.languages.setTextDocumentLanguage(doc, "javascript");
+		vscode.languages.setTextDocumentLanguage(doc, "javascript"); //Enables syntax highlighting
+	}
+
+	const getClonePilotText = (fns, word) => {
+		codelensProvider.clearPositions(); //Reset the codelens
+		let content = `/* Clone Pilot found ${fns.length} functions for ${word} */\n\n`;
+		for (let i = 0; i < fns.length; i++) {
+			const lineNum = content.split('\n').length; //The line to insert the codelens on
+			const formattedFn = formatFunction(fns[i]);
+			codelensProvider.addPosition(lineNum, fns[i]); //Add a codelens on that line
+			content += formattedFn.postHeader + formattedFn.keywords + formattedFn.header + formattedFn.body; //Display the entire function in the ClonePilot window
+			if (i < fns.length - 1) content += '\n\n';
+		}
+		return content;
 	}
 
 	const formatFunction = fn => {
@@ -72,7 +84,7 @@ function activate(context) {
 		const keywords = (fn.fnIsAsync ? 'async ' : '') + 'function ';
 		const header = fn.fnName + '(' + fn.fnParams.replace(/,/g, ', ') + ') ';
 		const body = (fn.fnIsExpression ? '{\n' : '') + fn.fnBody + (fn.fnIsExpression ? '\n}' : '');
-		//TODO Indentation is off
+		//TODO Indentation is messed up
 
 		return {
 			postHeader,
@@ -82,41 +94,40 @@ function activate(context) {
 		};
 	}
 
-	const chooseOption = vscode.commands.registerCommand('clone-pilot.chooseOption', fn => {
-		if (selectedEditor) {
-			try {
-				selectedEditor.edit(editBuilder => {
-					const formatted = formatFunction(fn);
-					editBuilder.replace(selectedRange, formatted.header + formatted.body);
-				});
-			} catch (e) {
-				//The editor isn't open
-			}
-		}
-		//Close clone pilot window. The hide function is deprecated, so it must be shown then closed as the active editor.
-		vscode.window.showTextDocument(myScheme, {
-				preview: true,
-				preserveFocus: false
-			})
-			.then(() => {
-				return vscode.commands.executeCommand('workbench.action.closeActiveEditor');
+	//When the user clicks on a codelens for a function
+	context.subscriptions.push(vscode.commands.registerCommand('clone-pilot.chooseOption', fn => {
+		if (!selectedEditor) return;
+		try {
+			selectedEditor.edit(editBuilder => {
+				const formatted = formatFunction(fn);
+				editBuilder.replace(selectedRange, formatted.header + formatted.body); //Insert the function into the text
 			});
-	});
-	context.subscriptions.push(chooseOption);
+			//Close the ClonePilot window. The hide function is deprecated, so it must be shown then closed as the active editor.
+			vscode.window.showTextDocument(myScheme, {
+					preview: true,
+					preserveFocus: false
+				})
+				.then(() => {
+					return vscode.commands.executeCommand('workbench.action.closeActiveEditor');
+				});
+		} catch (e) {
+			//The editor isn't open
+		}
+	}));
 
-	const codelensProvider = new class {
+	const codelensProvider = new class { //Keeps track of and provides codelenses
 		constructor() {
 			this.codelenses = [];
 		}
 		addPosition(lineNum, fn) {
-			const range = new vscode.Range(lineNum, 0, lineNum, 0);
+			const range = new vscode.Range(lineNum, 0, lineNum, 0); //Display it on that line
 			this.codelenses.push(new vscode.CodeLens(range, {
-				title: 'Choose option',
+				title: 'Use function',
 				command: 'clone-pilot.chooseOption',
 				arguments: [
 					fn
 				],
-				tooltip: 'Select'
+				tooltip: 'Insert this function into your code'
 			}));
 		}
 		clearPositions() {
@@ -129,10 +140,9 @@ function activate(context) {
 
 		//TODO Use resolveCodeLens() instead of making the command in addPosition?
 	}();
-
-	vscode.languages.registerCodeLensProvider({
-		scheme: myScheme //Only adds codelens to my scheme
-	}, codelensProvider); //TODO Make disposable
+	context.subscriptions.push(vscode.languages.registerCodeLensProvider({
+		scheme: myScheme //Only adds codelens to ClonePilot windows
+	}, codelensProvider));
 }
 
 function deactivate() {}
